ddb5b3a5db2234a450caa64a3868841e37fe0eac
beniamine3155 <beniamine3155@gmail.com>
2025-07-24 15:47:56 +0600

    groq chat model

---
+++ 01_Langchain_Models/02_ChatModels/02_groq_chatmodel.py
@@ -0,0 +1,8 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+load_dotenv()
+
+llm = ChatGroq(model="qwen/qwen3-32b", temperature=1.5)
+result = llm.invoke("What is the difference between chatModel and llm??")
+
+print(result)
