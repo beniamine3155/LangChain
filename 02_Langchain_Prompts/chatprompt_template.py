5bb2ddb274859efac4ab47aa01bd59d74c2f2087
beniamine3155 <beniamine3155@gmail.com>
2025-07-25 09:10:44 +0600

    typeddict structured output

--- 02_Langchain_Prompts/chatprompt_template.py
+++ 02_Langchain_Prompts/chatprompt_template.py
@@ -1,9 +1,9 @@
 from langchain_core.prompts import ChatPromptTemplate
 
-chat_template = ChatPromptTemplate({
+chat_template = ChatPromptTemplate([
    ('system', 'You are a helpful {domain} expert'),
    ('human', 'Explain the simple terms, what is {topic}')
-})
+])
 
 prompt = chat_template.invoke({'domain':'doctor', 'topic':'Cancer'})
 
